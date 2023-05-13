e27add5729afd6d02b1a369986b17138b2911fa2
rjsaurav13 <rjsaurav13@gmail.com>
2023-05-13 12:57:40 +0530

    mysql

---
+++ WIley_sql/basic_queries.sql
@@ -0,0 +1,11 @@
+show databases;
+create database wiley_sql;
+use wiley_sql;
+show tables;
+CREATE TABLE Persons (
+    rolno int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
