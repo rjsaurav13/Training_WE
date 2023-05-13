d477af01d54f94056e882ef84ef896d7f67a00c7
rjsaurav13 <rjsaurav13@gmail.com>
2023-05-13 12:59:09 +0530

    mysql

--- WIley_sql/basic_queries.sql
+++ WIley_sql/basic_queries.sql
@@ -1,11 +1,24 @@
 show databases;
 create database wiley_sql;
+create database temp;
+drop database temp;
 use wiley_sql;
 show tables;
-CREATE TABLE Persons (
+create table student (
     rolno int,
     LastName varchar(255),
     FirstName varchar(255),
     Address varchar(255),
     City varchar(255)
 );
+
+CREATE TABLE student2 (
+    roll int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+drop table student2;
+INSERT INTO student VALUES (44, 'Kumar', 'saurav', 'Indira Nagar', 'Dehradun');
+select * from student;
