7ab0ab290a1d24b631d9e0338e7ea5f1e266935e
rjsaurav13 <rjsaurav13@gmail.com>
2023-05-17 13:06:05 +0530

    mysql

--- WIley_sql/hr.sql
+++ WIley_sql/hr.sql
@@ -31,4 +31,23 @@ from employees
 INNER JOIN
 departments using(department_id);
 
-        
+SELECT * FROM tasks;
+DROP TABLE tasks;
+CREATE TABLE tasks(
+    id INT AUTO_INCREMENT,
+    task_name VARCHAR(100) NOT NULL UNIQUE,
+    lastName VARCHAR(100) NOT NULL,
+    starting_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id), FOREIGN KEY(id) REFERENCES employees(employeeNumber)
+    --FOREIGN KEY(lastName) REFERENCES employees(lastName)
+);
+ALTER TABLE tasks AUTO_INCREMENT=1000;
+
+SET foreign_key_checks = 0;
+ALTER TABLE tasks DISABLE KEYS;
+
+INSERT INTO tasks (task_name,lastName) VALUES ("Cooking", "Peterson");
+
+DESCRIBE employees;
+SELECT * FROM employees;
+SELECT * FROM tasks;
